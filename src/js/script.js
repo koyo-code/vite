abc7408ff6665ef7d12587cbed5deff200e9ad77
koyo code <koyo20010815@gmail.com>
2023-04-19 21:11:05 +0900

    vite-template

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+import gsap from "gsap";
+
+gsap.to(".aaa", 0.3, { opacity: 0.3 });
+document.querySelector(".aaa");
+$(".hello").on("click", function () {
+  $(".world").fadeToggle();
+});
